eacbf9b9d439ed056f1c62af4312194f19fbdbe5
Felix Gellner <flink@flinkblog.de>
2016-07-30 14:10:22 +0200

    changes to f****** rails stuff not working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
 
+
+  get '/stats' => 'pages#stats'
+  get '/about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
